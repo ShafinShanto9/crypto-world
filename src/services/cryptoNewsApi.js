8806fd66d3d4c1d5d83a1d66b06433b1ef8d949f
shafin_shant0 <shafinshanto9@gmail.com>
2022-02-13 18:43:25 +0600

    news page complete

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': 'ab708b55d1msh94387dcb213bf82p17604djsne3da4df75f50'
+};
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl}),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
