b127a7b51aa82aeb6208affbb2cb729559400ee1
shafin_shant0 <shafinshanto9@gmail.com>
2022-02-13 13:52:43 +0600

    components ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import {  Route, Link, Routes } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import "./App.css"
+import Homepage from './components/Homepage';
+import Exchanges from './components/Exchanges';
+import Cryptocurrencies from './components/Cryptocurrencies';
+import CryptoDetails from './components/CryptoDetails';
+import News from './components/News';
 
-function App() {
-  return (
+const App = () => {
+  return(
     <div className="App">
-     <h1>Crypto World</h1>
+     <div className='navbar'>
+      <Navbar></Navbar>
+     </div>
+     <div className='main'>
+     <Layout>
+        <div className="routes">
+          <Routes>
+            <Route path="/" element={<Homepage />}>
+            </Route>
+            <Route path="/exchanges" element={<Exchanges  />}>
+            </Route>
+            <Route path="/cryptocurrencies" element={<Cryptocurrencies />}>
+            </Route>
+            <Route path="/crypto/:coinId" element={<CryptoDetails  />}>
+            </Route>
+            <Route path="/news" element={<News  />}>
+            </Route>
+          </Routes>
+        </div>
+      </Layout>
+     </div>
+     <div className='footer'>
+
+     </div>
     </div>
-  );
-}
+  )
+};
 
 export default App;
+
